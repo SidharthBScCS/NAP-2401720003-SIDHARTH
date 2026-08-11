804af5a567381312687a2b8f96d5d3d29b58843a
SIDHARTH <codefather0003@gmail.com>
2026-08-11 10:02:52 +0530

    UPDATED CALCULATOR

--- GO/CALCULATOR/Calculator.go
+++ GO/CALCULATOR/Calculator.go
@@ -9,4 +9,11 @@ func main() {
 	fmt.Println("Subtraction: ", a, "-", b, "=", a-b)
 	fmt.Println("Multiplication: ", a, "*", b, "=", a*b)
 	fmt.Println("Division: ", a, "/", b, "=", a/b)
+
+	var x float32 = 3.45
+	var y float32 = 4.45
+	fmt.Println("Addition: ", x, "+", y, "=", x+y)
+	fmt.Println("Subtraction: ", x, "-", y, "=", x-y)
+	fmt.Println("Multiplication: ", x, "*", y, "=", x*y)
+	fmt.Println("Division: ", x, "/", y, "=", x/y)
 }
