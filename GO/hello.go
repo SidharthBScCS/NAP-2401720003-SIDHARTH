0f340684fd1874975f1419d2724a7fac8743d0b7
SIDHARTH <codefather0003@gmail.com>
2026-08-11 09:50:26 +0530

    CALCULATOR.GO

--- GO/hello.go
+++ GO/hello.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	name := "Sidharth"
-	roll := 0003
-	fmt.Printf("Name:%s\nRollNo:%d", name, roll)
+	var score int = 89
+
+	if score >= 60 {
+		fmt.Println("PASS")
+	} else if score == 0 {
+		fmt.Println("GET OUT")
+	} else {
+		fmt.Println("FAIL")
+	}
 }
