27bd9c0c9bd48738d3c3913f21987ac2ba2e9908
SIDHARTH <codefather0003@gmail.com>
2026-08-11 09:34:04 +0530

    First commit

---
+++ GO/Calculator.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 20
+	fmt.Println("Addition: ", a, "+", b, "=", a+b)
+	fmt.Println("Subtraction: ", a, "-", b, "=", a-b)
+	fmt.Println("Multiplication: ", a, "*", b, "=", a*b)
+	fmt.Println("Division: ", a, "/", b, "=", a/b)
+}
---
+++ GO/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Sidharth"
+	roll := 0003
+	fmt.Printf("Name:%s\nRollNo:%d", name, roll)
+}
